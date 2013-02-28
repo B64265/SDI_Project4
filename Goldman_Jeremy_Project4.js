77d6417c88e16ff456690e190feab7a32e250898
Jeremy Goldman <jeremygoldman@fullsail.edu>
2013-02-27 19:04:48 -0600

    checkNumeric Function
    Check if the passed value is a number.

--- Goldman_Jeremy_Project4.js
+++ Goldman_Jeremy_Project4.js
@@ -1,1 +1,24 @@
-alert("JavaScript works!");
+//Jeremy Goldman
+// SDI Term:1302
+// Project 4
+// This Library is gonna rock ;)
+
+var myLibrary = function(){
+	
+	// Check Numeric Function
+	var checkNumeric = function(val){
+		
+		if(isNaN(val)){
+			
+			return false;
+			
+		}else{
+			
+			return true;
+			
+		}
+		
+	}
+	
+		
+}
