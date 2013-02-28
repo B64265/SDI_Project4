88022495af07c737e41a34ca57f9b67afc6b090e
Jeremy Goldman <jeremygoldman@fullsail.edu>
2013-02-27 21:12:53 -0600

    CheckLength + Outputs
    I added a checkLength function to ensure that the length of the phone
    number matches the correct number of characters, before we check for
    the correct characters.

--- Goldman_Jeremy_Project4.js
+++ Goldman_Jeremy_Project4.js
@@ -20,5 +20,53 @@ var myLibrary = function(){
 		
 	}
 	
+	
+	//Check length
+	var checkLength = function(val, desiredLength){
+	
+		if(val.length === desiredLength){
+			
+			return true;
+			
+		}else{
+			
+			return false;
+		}
 		
+	}
+	
+	// Check String Function
+	
+	var checkString = function(val){
+	
+		var strLen = val.length;
+		for(var i = 0; i <=strLen; i++){
+			
+			if(val.substring(i, i+1) === "-"){
+				
+				return i;
+			}
+			
+		}
+	
+	
+	}
+	
+	
+	
+	// Return Object
+	return{
+		"checkNumeric": checkNumeric,
+		"checkLength": checkLength
+		
+	}
 }
+
+
+var newLib = new myLibrary();
+
+console.log("Is this a number? "+newLib.checkNumeric(3));
+
+console.log("Does this length match the desired length? " + newLib.checkLength("224-366-7016", 12));
+
+
